64be64f2ba4b2794e4941cd25f3a6780abfb9d7c
DarrianTalamantes <darrianrtalamantes6@gmail.com>
2023-09-15 17:18:02 -0400

    trying to get sapelo to work

--- workflow/sapelo_submission.sh
+++ workflow/sapelo_submission.sh
@@ -17,9 +17,6 @@ echo "Done."
 
 sacct -j $SLURM_JOB_ID --format=JobID,JobName,AllocCPUS,Elapsed,ExitCode,State,MaxRSS,TotalCPU
 
-module load Anaconda3/2022.10
-module load snakemake/7.22.0-foss-2022a
-
 source activate snakemake
 
 export LC_ALL=en_SG.utf8

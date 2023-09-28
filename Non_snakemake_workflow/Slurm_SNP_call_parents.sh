e9c268c5348febf69965f30326c0ac2526c9e2cf
DarrianTalamantes <darrianrtalamantes6@gmail.com>
2023-09-28 16:11:58 -0400

    running shortened files

--- Non_snakemake_workflow/Slurm_SNP_call_parents.sh
+++ Non_snakemake_workflow/Slurm_SNP_call_parents.sh
@@ -14,4 +14,4 @@ RefGenome="/scratch/drt83172/Wallace_lab/Mapping_and_QTL/Mapping_and_QTL/Data/Re
 Bam_Files="/scratch/drt83172/Wallace_lab/Mapping_and_QTL/Mapping_and_QTL/Data/Real_Data/Lists/bam_list_parents_shortened.txt"
 VCF_loc="/scratch/drt83172/Wallace_lab/Mapping_and_QTL/Mapping_and_QTL/Data/Real_Data/VCF"
 
-bcftools mpileup --threads 12 -f $RefGenome -b $Bam_Files | bcftools call --threads 32 -mv -Ob -o $VCF_loc/Variants_Parents.bcf
+bcftools mpileup --threads 12 -f $RefGenome -b $Bam_Files | bcftools call --threads 12 -mv -Ob -o $VCF_loc/Variants_Parents.bcf

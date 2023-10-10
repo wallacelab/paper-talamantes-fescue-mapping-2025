7d161a88f80ea0062a342cfb44c0b0c34dfbc2c8
DarrianTalamantes <darrianrtalamantes6@gmail.com>
2023-10-10 15:31:11 -0400

    made a depth analyzer

--- Non_snakemake_workflow/Slurm_Depth_call_all.sh
+++ Non_snakemake_workflow/Slurm_Depth_call_all.sh
@@ -9,9 +9,9 @@
 #SBATCH --mail-user=drt83172@uga.edu
 #SBATCH --mail-type=FAIL,END
 
-module load BCFtools/1.15.1-GCC-11.3.0
+module load SAMtools/1.16.1-GCC-11.3.0
 RefGenome="/scratch/drt83172/Wallace_lab/Mapping_and_QTL/Mapping_and_QTL/Data/Real_Data/Genome/tall_fescuev0.1.fa"
 Bam_Files="/scratch/drt83172/Wallace_lab/Mapping_and_QTL/Mapping_and_QTL/Data/Real_Data/Lists/bam_list.txt"
 VCF_loc="/scratch/drt83172/Wallace_lab/Mapping_and_QTL/Mapping_and_QTL/Data/Real_Data/VCF"
 
-bcftools depth --threads 12 -f $Bam_Files $VCF_loc/depths.bcf
+samtools depth --threads 12 -f $Bam_Files $VCF_loc/depths.bcf

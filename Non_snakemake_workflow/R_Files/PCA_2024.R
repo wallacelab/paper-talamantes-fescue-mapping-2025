e994d47cb89e90bf021a6ccdae1136bc87283638
Darrian <darrianrtalamantes6@gmail.com>
2024-10-11 19:06:44 -0400

    made more graphs based off tassel data

--- Non_snakemake_workflow/R_Files/PCA_2024.R
+++ Non_snakemake_workflow/R_Files/PCA_2024.R
@@ -7,6 +7,10 @@ library(factoextra)
 library(patchwork)
 library(gridExtra)
 library(grid)
+library(vegan)
+library(ape)
+library(ggrepel)
+
 
 # Loading in the data
 phenotypes_2024_loc <- "/home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Phenotype_Data/2024_Data/Final_2024_phenotype_data.csv"
@@ -32,8 +36,9 @@ phenotypes_2024$ID <- gsub("-", "_", phenotypes_2024$ID)
 
 
 
-
+####
 # Creating a PCA function
+####
 PCA_plotter <- function(vcf_file, phenotypes, title){
   # Step 2: Extract the genotype matrix (individuals as columns)
   genotype_matrix <- extract.gt(vcf_file, as.numeric = TRUE)
@@ -91,7 +96,9 @@ PCA_plotter <- function(vcf_file, phenotypes, title){
     scale_color_brewer(palette = "Set1") 
   return(pca_plot)
 }
-
+#####
+# End Function
+#####
 
 PCA_314x310 <- PCA_plotter(vcf_314x310, phenotypes_2024, "PCA 314x310")
 PCA_314x310
@@ -140,99 +147,138 @@ grid.arrange(
 
 
 
-# Step 2: Extract the genotype matrix (individuals as columns)
-genotype_matrix <- extract.gt(vcf_314x312, as.numeric = TRUE)
-genotype_matrix[is.na(genotype_matrix)] <- 0  # Handle missing data
-
-# Step 3: Transpose the genotype matrix so that individuals are in rows
-genotype_df <- as.data.frame(t(genotype_matrix))
-
-# Assuming you have a phenotype data frame with IDs and crosses
-colnames(phenotypes_2024)
-phenotypes_2024$ID <- gsub("-", "_", phenotypes_2024$ID)
-phenotypes_2024$cross <- apply(phenotypes_2024[, c("Mother", "Father")], 1, function(x) paste(sort(x), collapse = "x"))
-
-# Step 5: Set row names to match IDs from the phenotype data
-genotype_ids <- rownames(genotype_df)
-phenotypes_314x310 <- phenotypes_2024 %>%
-  filter(ID %in% genotype_ids)
-phenotypes_314x310 <- unique(phenotypes_314x310)
-
-# Ensure there are no extra phenotypes without corresponding genotype IDs
-unique_genotype_ids <- unique(rownames(genotype_df))
-unique_phenotype_ids <- unique(phenotypes_314x310$ID)
-common_ids <- intersect(unique_phenotype_ids, unique_genotype_ids)
-# Filter both datasets to only include matching IDs
-
-genotype_df_filtered <- genotype_df[rownames(genotype_df) %in% common_ids, ]
-phenotypes_filtered <- phenotypes_314x310[phenotypes_314x310$ID %in% common_ids, ]
-rownames(genotype_df_filtered) <- phenotypes_filtered$ID
-
-# Step 6: Perform PCA on the transposed genotype data (individuals in rows)
-constant_columns <- sapply(genotype_df, function(x) length(unique(x)) == 1)
-genotype_df_filtered <- genotype_df[, !constant_columns]
-ncol(genotype_df_filtered)
-ncol(genotype_df)
-pca_result <- prcomp(genotype_df_filtered, center = TRUE, scale. = TRUE)
-
-# Step 7: Prepare PCA results for visualization
-pca_data <- as.data.frame(pca_result$x)
-
-# Add the 'cross' information to the PCA data
-pca_data$cross <- phenotypes_2024$cross[match(rownames(pca_data), phenotypes_2024$ID)]
-
-# Step 8: Create PCA plot
-ggplot(pca_data, aes(x = PC1, y = PC2, color = cross)) +
-  geom_point(size = 3) +
-  labs(title = "PCA of Individuals Colored by Cross", x = "Principal Component 1", y = "Principal Component 2") +
-  theme_minimal() +
-  scale_color_brewer(palette = "Set1")
+  # Step 2: Extract the genotype matrix (individuals as columns)
+ genotype_matrix <- extract.gt(vcf_314x310_filtered, as.numeric = TRUE)
+ genotype_df <- as.data.frame(t(genotype_matrix))
+ distance_matrix <- vegdist(genotype_matrix, method = "euclidean", na.rm = TRUE)
+ pcoa_result <- pcoa(distance_matrix)
+ 
+  
+  # Assuming you have a phenotype data frame with IDs and crosses
+  colnames(phenotypes_2024)
+  phenotypes_2024$ID <- gsub("-", "_", phenotypes_2024$ID)
+  phenotypes_2024$cross <- apply(phenotypes_2024[, c("Mother", "Father")], 1, function(x) {
+    if (all(is.na(x) | x == "")) {
+      return("Parent")
+    } else {
+      return(paste(sort(x), collapse = "x"))
+    }
+  })  
+  # Step 5: Set row names to match IDs from the phenotype data
+  genotype_ids <- rownames(genotype_df)
+  phenotypes_unique <- phenotypes_2024 %>%
+    filter(ID %in% genotype_ids)
+  phenotypes_unique <- unique(phenotypes_unique)
+  
+  # Ensure there are no extra phenotypes without corresponding genotype IDs
+  unique_genotype_ids <- unique(rownames(genotype_df))
+  unique_phenotype_ids <- unique(phenotypes_unique$ID)
+  common_ids <- intersect(unique_phenotype_ids, unique_genotype_ids)
+  # Filter both datasets to only include matching IDs
+  
+  genotype_df_filtered <- genotype_df[rownames(genotype_df) %in% common_ids, ]
+  phenotypes_filtered <- phenotypes_unique[phenotypes_unique$ID %in% common_ids, ]
+  rownames(genotype_df_filtered) <- phenotypes_filtered$ID
+  
+  # Step 6: Perform PCA on the transposed genotype data (individuals in rows)
+  constant_columns <- sapply(genotype_df, function(x) length(unique(x)) == 1)
+  genotype_df_filtered <- genotype_df[, !constant_columns]
+  ncol(genotype_df_filtered)
+  ncol(genotype_df)
+  pca_result <- prcomp(genotype_df_filtered, center = TRUE, scale. = TRUE)
+  
+  # Step 7: Prepare PCA results for visualization
+  pca_data <- as.data.frame(pca_result$x)
+  
+  # Add the 'cross' information to the PCA data
+  pca_data$cross <- phenotypes_2024$cross[match(rownames(pca_data), phenotypes_2024$ID)]
+  
+  # Step 8: Create PCA plot
+  pca_plot <- ggplot(pca_data, aes(x = PC1, y = PC2, color = cross)) +
+    geom_point(size = 3) +
+    ggtitle(title) +
+    labs(x = "Principal Component 1", y = "Principal Component 2") +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    scale_color_brewer(palette = "Set1") 
 
 
 ################################################################################
 # ME being stupid and doing a PCA on the phenotypes
 ###############################################################################
 
+# 
+# 
+# 
+# # Finding the cross
+# phenotypes_2024$cross <- apply(phenotypes_2024[, c("Mother", "Father")], 1, function(x) {
+#   if (all(is.na(x)) || all(x == "")) {
+#     return("Parent")
+#   } else {
+#     return(paste(sort(x), collapse = "x"))
+#   }
+# })
+# 
+# 
+# colnames(phenotypes_2024)
+# phenotype_data <- subset(phenotypes_2024, select = c("Delta_CT_OG","ng.g"))
+# 
+# 
+# # Makes the data more normal 
+# pca_result <- prcomp(phenotype_data, scale. = TRUE)  # Scaling the data is important
+# pca_data <- as.data.frame(pca_result$x)
+# 
+# # Adding catagorical data back into the data
+# pca_data$cross <- phenotypes_2024$cross
+# pca_data$Data_Set <- phenotypes_2024$Data_Set
+# pca_data$Alkaloid_Plate <- phenotypes_2024$Alkaloid_Plate
+# 
+# # PCA 
+# ggplot(pca_data, aes(x = PC1, y = PC2, color = Alkaloid_Plate)) +
+#   geom_point() +
+#   labs(title = "PCA of Numerical Data Colored by Category") +
+#   theme_minimal()
+
+################################################################################
+# Graphs from tassel data
+################################################################################
+PCA_MCR50_Tassel_loc <- "/home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/2024_only/PCA_MCR50.txt"
+PCA_MCR50_Tassel <- read.csv(PCA_MCR50_Tassel_loc, header = FALSE, strip.white=TRUE, skip = 2)
+
+PCA_MCR50_Tassel_filtered_loc <- "/home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/2024_only/PCA_MCR50_fitlered.txt"
+PCA_MCR50_Tassel_filtered <- read.csv(PCA_MCR50_Tassel_filtered_loc, header = FALSE, strip.white=TRUE, skip = 2)
+
+PCA_MCR50_314x312_loc <- "/home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/2024_only/PCA_MCR50_314x312.txt"
+PCA_MCR50_314x312 <- read.csv(PCA_MCR50_314x312_loc, header = FALSE, strip.white=TRUE, skip = 2)
+
+PCA_filtered_314x312_loc <- "/home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/2024_only/PCA_all_314x312.txt"
+PCA_filtered_314x312 <- read.csv(PCA_filtered_314x312_loc, header = FALSE, strip.white=TRUE, skip = 2)
 
 
 
-# Finding the cross
-phenotypes_2024$cross <- apply(phenotypes_2024[, c("Mother", "Father")], 1, function(x) {
-  if (all(is.na(x)) || all(x == "")) {
-    return("Parent")
-  } else {
-    return(paste(sort(x), collapse = "x"))
-  }
-})
+PCA_tassel_data <- function(Tassel_PCA, title){
+  Tassel_PCA <- Tassel_PCA[, -((ncol(Tassel_PCA)-4):ncol(Tassel_PCA))]
+  colnames(Tassel_PCA) <- Tassel_PCA[1, ]
+  Tassel_PCA <- Tassel_PCA[-1, ]
+  Tassel_PCA$PC1 <- as.numeric(as.character(Tassel_PCA$PC1))
+  Tassel_PCA$PC2 <- as.numeric(as.character(Tassel_PCA$PC2))
+  
+  # Making ggplot with data
+  TasselPlot <- ggplot(Tassel_PCA, aes(x = PC1, y = PC2, color = Cross)) +
+    geom_point() +
+    labs(title = "Tassel MCR50 PCA not filtered") +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    geom_text_repel(data = subset(as.data.frame(Tassel_PCA), Tassel_PCA$Cross == "parent"),
+                    aes(label = Taxa),
+                    max.overlaps = Inf,   
+                    nudge_x = 0.1, 
+                    nudge_y = 0.1)
+  return(TasselPlot)
+}
 
-
-colnames(phenotypes_2024)
-phenotype_data <- subset(phenotypes_2024, select = c("Delta_CT_OG","ng.g"))
-
-
-# Makes the data more normal 
-pca_result <- prcomp(phenotype_data, scale. = TRUE)  # Scaling the data is important
-pca_data <- as.data.frame(pca_result$x)
-
-# Adding catagorical data back into the data
-pca_data$cross <- phenotypes_2024$cross
-pca_data$Data_Set <- phenotypes_2024$Data_Set
-pca_data$Alkaloid_Plate <- phenotypes_2024$Alkaloid_Plate
-
-# PCA 
-ggplot(pca_data, aes(x = PC1, y = PC2, color = Alkaloid_Plate)) +
-  geom_point() +
-  labs(title = "PCA of Numerical Data Colored by Category") +
-  theme_minimal()
-
-
-
-
-
-
-
-
-
-
-
+PCA_tassel_data(PCA_MCR50_Tassel)
+PCA_tassel_data(PCA_MCR50_Tassel_filtered)
+PCA_tassel_data(PCA_MCR50_314x312)
+PCA_tassel_data(PCA_filtered_314x312)
 

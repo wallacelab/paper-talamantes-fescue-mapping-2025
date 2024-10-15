c03513442fad0dc03eed85d0f0686e1986b7ec29
Darrian <darrianrtalamantes6@gmail.com>
2024-10-15 16:59:10 -0400

    Adjusted vcf filtering parameters

--- Non_snakemake_workflow/VCF_FIltering_2024/Filter_2024_VCF.sh
+++ Non_snakemake_workflow/VCF_FIltering_2024/Filter_2024_VCF.sh
@@ -5,35 +5,49 @@ list_314x310=/home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Lists/Pa
 vcf_file=/home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/Tall.fescue.all.snps.vcf.gz
 vcf_mcr50=/home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/Tall.fescue.MCR50.snps.vcf.gz
 
-Major=0.85
+MAF=0.05
 missing=43
 depth=696
-# Filters I am using
-echo "filtering MAF at $MAF"
-echo "filtering missing at $missing"
-echo "filtering depth at $depth" 
+maxdepth=2610
+
+
+###################################
+# This extracts populations from larger files
+###################################
+
+echo
+echo "creating subseted data for 314x312"
+echo 
 bcftools view -s `cat $list_314x312 | tr '\n' ',' | sed 's/,$//'` $vcf_file -o /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x312.vcf
 bcftools view -s `cat $list_314x312 | tr '\n' ',' | sed 's/,$//'` $vcf_mcr50 --force-samples -o /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/MCR50_snps_314x312.vcf
 
 
-echo "filtering MAF at $MAF"
-echo "filtering missing at $missing"
-echo "filtering depth at $depth" 
+echo
+echo "creating subseted data for 314x310"
+echo 
 bcftools view -s `cat $list_314x310 | tr '\n' ',' | sed 's/,$//'` $vcf_file -o /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x310.vcf
 bcftools view -s `cat $list_314x310 | tr '\n' ',' | sed 's/,$//'` $vcf_mcr50 --force-samples -o /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/MCR50_snps_314x310.vcf
 
-echo "starting $list_314x312"
+##################################
+# Filters extracted data and the all snps file with my filters
+##################################
+echo 
+echo "############################################################################"
+echo "filtering MAF at $MAF"
+echo "filtering missing at $missing"
+echo "filtering depth at $depth and max of $maxdepth"
+echo "############################################################################"
+echo 
+
+echo "Filtering $list_314x312"
 # bcftools view -S $list_314x312 $vcf_file -o /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x312.vcf
-vcftools --vcf /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x312.vcf --maf 0.15 --max-missing 0.5 --minDP 8 --recode --out /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x312_filtered
+vcftools --vcf /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x312.vcf --maf $MAF --max-missing $missing --minDP $depth --maxDP $maxdepth --recode --out /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x312_filtered
 
-echo "starting $list_314x310"
-vcftools --vcf /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x310.vcf --maf 0.15 --max-missing 0.5 --minDP 8 --recode --out /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x310_filtered
+echo "Filtering $list_314x310"
+vcftools --vcf /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x310.vcf --maf $MAF --max-missing $missing --minDP $depth --maxDP $maxdepth --recode --out /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x310_filtered
 
-echo "starting $vcf_mcr50"
-vcftools --vcf $vcf_mcr50 --maf 0.15 --max-missing 0.5 --minDP 8 --recode --out /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/MCR50_Extra_filtered
-
-echo "starting $vcf_file"
-vcftools --vcf $vcf_file --maf 0.15 --max-missing 0.5 --minDP 8 --recode --out /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_filtered
+echo "Filtering $vcf_file"
+vcftools --vcf $vcf_file --maf $MAF --max-missing $missing --minDP $depth --maxDP $maxdepth --recode --out /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_filtered
 
 rm /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x310.vcf
 rm /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x312.vcf
@@ -59,35 +73,13 @@ vcftools --gzvcf /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/
 # Below is me running the R program that makes summary stats for my VCF files
 ######
 
-./1c_PlotGenoSummary.r -d /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/all_snps_filtered_site_depth.ldepth -g /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/all_snps_filtered_depth.gdepth -t /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/all_snps_filtered_TaxaSummary.txt -s /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/all_snps_filtered_SiteSummary.txt -o /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/all_snps_filtered.png
+#./1c_PlotGenoSummary.r -d /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/all_snps_filtered_site_depth.ldepth -g /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/all_snps_filtered_depth.gdepth -t /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/all_snps_filtered_TaxaSummary.txt -s /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/all_snps_filtered_SiteSummary.txt -o /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/all_snps_filtered.png
 
-./1c_PlotGenoSummary.r -d /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/MCR50_site_depth.ldepth -g /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/MCR50_depth.gdepth -t /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/MCR50_TaxaSummary.txt -s /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/MCR50_SiteSummary.txt -o /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/MCR50_stats.png
-
-
+# ./1c_PlotGenoSummary.r -d /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/MCR50_site_depth.ldepth -g /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/MCR50_depth.gdepth -t /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/MCR50_TaxaSummary.txt -s /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/MCR50_SiteSummary.txt -o /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/Tassel_Outputs/files_for_site_analysis/MCR50_stats.png
 
 
 
-################ Below is kinda useless tbh ########################################
 
-
-# echo "bcftools view -i INFO/AF[0] > $Major && INFO/DP > $depth && F_MISSING && INFO/NS >= $missing /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x312.vcf -o /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x312_filtered.vcf"
-# bcftools view -i "INFO/AF[0] <= $Major && INFO/DP >= $depth && F_MISSING && INFO/NS >= $missing" /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x312.vcf -o /home/darrian/Desktop/UGA/Wallace_Lab/Mapping_and_QTL/Data/VCF/all_snps_314x312_filtered.vcf
-
-
-########## Examples of filtering with bcftools
-# To filter by depth we have 87 samples in cross 312x314. For avg depth of 10 we would use 870
-# -i 'INFO/DP > 870'
-
-# Major allel frequency filter 
-# 'INFO/AF[0] > 0.85'
-
-# Get rid of genotypes missing more than 50% 43/87
-# -i 'INFO/NS >= 43' 
-
-# Keep only bi allilic sites
-# -m2 -M2
-
-# bcftools view -s $list_314x310
 
 
 

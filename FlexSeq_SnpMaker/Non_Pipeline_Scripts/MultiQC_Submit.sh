58fec9128a78ec03a09cd02de99dcefd4122bb67
Darrian <darrianrtalamantes6@gmail.com>
2025-03-17 10:20:09 -0400

    multiqc

---
+++ FlexSeq_SnpMaker/Non_Pipeline_Scripts/MultiQC_Submit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -J MultiQC
+#SBATCH -p batch
+#SBATCH --ntasks=12
+#SBATCH --mem 50gb
+#SBATCH -t 140:00:00
+#SBATCH --output=/scratch/drt83172/Wallace_lab/Mapping_and_QTL/Mapping_and_QTL/Outfiles/MultiQC.%j.out
+#SBATCH -e /scratch/drt83172/Wallace_lab/Mapping_and_QTL/Mapping_and_QTL/Outfiles/MultiQC.%j.err
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user drt83172@uga.edu
+
+echo "This JobID for this job is ${SLURM_JOB_ID}."
+sleep 5
+echo "Done."
+
+sacct -j $SLURM_JOB_ID --format=JobID,JobName,AllocCPUS,Elapsed,ExitCode,State,MaxRSS,TotalCPU
+
+module load MultiQC/1.14-foss-2022a
+
+multiqc .

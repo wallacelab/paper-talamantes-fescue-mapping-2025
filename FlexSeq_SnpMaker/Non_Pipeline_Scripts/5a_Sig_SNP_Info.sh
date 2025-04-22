c3dd14218cb199987f77ba692df1ec0ff45a9edd
Darrian <darrianrtalamantes6@gmail.com>
2025-04-22 15:41:28 -0400

    SNP scripts

--- FlexSeq_SnpMaker/Non_Pipeline_Scripts/5a_Sig_SNP_Info.sh
+++ FlexSeq_SnpMaker/Non_Pipeline_Scripts/5a_Sig_SNP_Info.sh
@@ -15,8 +15,9 @@ samtools faidx tall_fescue_pv1.1.fasta FaChr7G1:214301125-214321125 > SNP_FACHR7
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/019/359/855/GCF_019359855.2_Kyuss_2.0/GCF_019359855.2_Kyuss_2.0_genomic.gff.gz
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/019/359/855/GCF_019359855.2_Kyuss_2.0/GCF_019359855.2_Kyuss_2.0_genomic.fna.gz
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/019/359/855/GCF_019359855.2_Kyuss_2.0/GCF_019359855.2_Kyuss_2.0_protein.faa.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/034/140/825/GCF_034140825.1_ASM3414082v1/GCF_034140825.1_ASM3414082v1_protein.faa.gz 
 
-
+# after files a gunzip you run this
 makeblastdb -in GCF_019359855.2_Kyuss_2.0_genomic.fna -dbtype nucl -out lolium_perenne_Kyuss
 blastn -query SNP_FACHR7G1_214311125_sequence.fa -db lolium_perenne_Kyuss -out results.out -outfmt 6
 
@@ -24,8 +25,13 @@ blastn -query SNP_FACHR7G1_214311125_sequence.fa -db lolium_perenne_Kyuss -out r
 # Protein Blast
 makeblastdb -in GCF_019359855.2_Kyuss_2.0_protein.faa -dbtype prot -out lp_Kyuss_protein_db
 blastx -query SNP_FACHR7G1_214311125_sequence.fa -db lp_Kyuss_protein_db -outfmt "6 qseqid sseqid stitle pident length evalue bitscore" -evalue 1e-5 -out protein_hits.tsv
+blastx -query SNP_FACHR7G1_214311125_sequence10k.fa -db lp_Kyuss_protein_db -outfmt "6 qseqid sseqid stitle pident length evalue bitscore" -evalue 1e-5 -out protein_hits_10k.tsv
 
 
+# Rice Protein Blast
+makeblastdb -in GCF_034140825.1_ASM3414082v1_protein.faa  -dbtype prot -out rice_db
+blastx -query SNP_FACHR7G1_214311125_sequence.fa -db rice_db -outfmt "6 qseqid sseqid stitle pident length evalue bitscore" -evalue 1e-5 -out protein_hits_rice.tsv
+blastx -query SNP_FACHR7G1_214311125_sequence10k.fa -db rice_db -outfmt "6 qseqid sseqid stitle pident length evalue bitscore" -evalue 1e-5 -out protein_hits_rice_10k.tsv
 
 
 
